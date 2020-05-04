ce42549fea39794f5f8ae1a49bf353c37cd14b3c
Matis Grinbaum <matis@MacBook-Pro-de-Matis.local>
2020-05-04 22:52:07 +0200

    version

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,49 +2,49 @@ import React from "react";
 
 const Content = ({ restaurant }) => {
   const menu = restaurant.categories;
-  console.log(menu);
 
   return (
-    <div className="content">
-      <div className="order container">
-        <div className="menu">
-          {menu.slice(0, 6).map((meal, index) => {
-            const meals = meal.meals;
-            return (
-              <div key={index}>
-                <div>
-                  <h1>{meal.name}</h1>
-                </div>
-                {/* <div>{meal.meals[index].title}</div> */}
-                {meals.map((meal, index) => {
-                  return (
-                    <div key={index}>
-                      <div>{meal.title}</div>
-                      <div>
-                        <h2>{meal.description}</h2>
-                      </div>
-                      <div>
-                        <h2>{meal.price} €</h2>
-                        {meal.popular ? (
-                          <div>
-                            <h2>Populaire</h2>
+    <div className="container all">
+      <div className="content">
+        {menu.slice(0, 6).map((meal, index) => {
+          const meals = meal.meals;
+          return (
+            <div className="menu-items" key={index}>
+              <div>
+                <h3>{meal.name}</h3>
+                <div className="flex">
+                  {meals.map((meal) => {
+                    return (
+                      <div className="item" key={meal.id}>
+                        <div className="dishes">
+                          <h4>{meal.title}</h4>
+                          <p>{meal.description}</p>
+                          <div className="price">
+                            <span>{meal.price} €</span>
+                            {meal.popular ? (
+                              <span className="pop"> Populaire</span>
+                            ) : null}
+                          </div>
+                        </div>
+                        {meal.picture ? (
+                          <div className="in-dish">
+                            <img
+                              className="dish"
+                              alt="dish"
+                              src={meal.picture}
+                            />
                           </div>
                         ) : null}
                       </div>
-                      {meal.picture ? (
-                        <div>
-                          <img className="dish" alt="dish" src={meal.picture} />
-                        </div>
-                      ) : null}
-                    </div>
-                  );
-                })}
+                    );
+                  })}
+                </div>
               </div>
-            );
-          })}
-        </div>
-        <div className="cart">Valider mon panier</div>
+            </div>
+          );
+        })}
       </div>
+      <div className="cart">Valider mon panier</div>
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,22 @@ import React from "react";
 
 const Header = ({ logo, restaurant }) => {
   return (
-    <div className="container header">
-      <div className="logo">
-        <img alt="logo" src={logo} />
-      </div>
-      <div className="info">
-        <div>
-          <h1>{restaurant.restaurant.name}</h1>
-          <h2>{restaurant.restaurant.description}</h2>
+    <div className="back">
+      <div className="container header">
+        <div className="logo">
+          <img alt="logo" src={logo} />
         </div>
-        <img className="meal" alt="meal" src={restaurant.restaurant.picture} />
+        <div className="info">
+          <div>
+            <h1>{restaurant.restaurant.name}</h1>
+            <h2>{restaurant.restaurant.description}</h2>
+          </div>
+          <img
+            className="meal"
+            alt="meal"
+            src={restaurant.restaurant.picture}
+          />
+        </div>
       </div>
     </div>
   );
